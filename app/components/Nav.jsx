349bc7f099b5c36159d3a405dcd978fad580271c
Shashank <saishashank.rasamalla@cognizant.com>
2017-04-09 16:44:58 -0500

    styling the nav bar

--- app/components/Nav.jsx
+++ app/components/Nav.jsx
@@ -1,30 +1,45 @@
-import React from  'react';
+import React from 'react';
 //var {Link} = require('react-router');
 import {Link, IndexLink} from 'react-router';
 
-/*var Nav = React.createClass({
+var Nav = React.createClass({
+    onSearch: function (e) {
+        e.preventDefault();
+        alert('Not Wired Up Yet !');
+    },
     render: function () {
         return (
-            <div>
-            <h2>Navigation Component</h2>
-            <IndexLink to="/" activeClassname="active">Get Weather </IndexLink>
-            <Link to="/about" activeClassname="active">About</Link>
-            <Link to="/examples" activeClassname="active">Examples</Link>
+            <div className="top-bar">
+                <div className="topbar-left">
+                    <ul className="menu">
+                        <li className="menu-text">React Weather App</li>
+                        <li>
+                            <IndexLink to="/" activeClassname="active">Get Weather</IndexLink>
+                        </li>
+                        <li>
+                            <Link to="/about" activeClassname="active">About</Link>
+                        </li>
+                        <li>
+                            <Link to="/examples" activeClassname="active">Examples</Link>
+                        </li>
+                    </ul>
+                </div>
+                <div className="top-bar-right">
+                    <form onSubmit={this.onSearch}>
+                        <ul className="menu">
+                            <li>
+                                <input type="search" placeholder="search weather"/>
+                            </li>
+                            <li>
+                                <input type="submit" className="button" value="Get Weather"/>
+                            </li>
+                        </ul>
+                    </form>
+                </div>
             </div>
         );
     }
 
-});*/
-
-var Nav =(props) =>{
-    return (
-            <div>
-            <h2>Navigation Component</h2>
-            <IndexLink to="/" activeClassname="active">Get Weather </IndexLink>
-            <Link to="/about" activeClassname="active">About</Link>
-            <Link to="/examples" activeClassname="active">Examples</Link>
-            </div>
-        );
-};
+});
 
 module.exports = Nav;
