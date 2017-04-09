2dcb47167ac33bfbef46a3857fc8907bbfeab789
Shashank <saishashank.rasamalla@cognizant.com>
2017-04-09 17:31:49 -0500

    updating about page and adding resource links

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,17 +1,18 @@
 import React from 'react';
 
-
-// var About = React.createClass({
-//     render : function () {
-//         return (
-//         <h3>About</h3>
-//         );
-//     }
-// });
-
 var About = () =>{
      return (
-    <h3>About Component</h3>
+         <div>
+    <h1 className="text-center">About</h1>
+    <p>This is a simple weather application built on React and foundation</p>
+    <p>Here are some of the resources used for this project</p>
+    <ul>
+        <li> <a href="https://facebook.github.io/react">React</a> This is the javascript framework used for this project
+            </li>
+            <li> <a href="http://openweathermap.org">Open Weather Map</a> This is the weather api used for this project
+            </li>
+        </ul>
+    </div>
    );
 };
 
